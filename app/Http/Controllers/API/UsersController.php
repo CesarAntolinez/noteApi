efd9353cff0173c24a49bddf66d653231e0a4804
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2020-03-07 13:09:51 -0500

    detalles de la sesion y login listo

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -42,4 +42,31 @@ class UsersController extends Controller
         return response()->json(['success'=>$success], $this->successStatus);
     }
 
+    /**
+     * details api
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function details()
+    {
+        $user = Auth::user();
+        return response()->json(['success' => $user], $this->successStatus);
+    }
+
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(){
+        if(Auth::attempt(['identification_card' => request('identification_card'), 'password' => request('password')])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')-> accessToken;
+            return response()->json(['success' => $success], $this-> successStatus);
+        }
+        else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
 }
