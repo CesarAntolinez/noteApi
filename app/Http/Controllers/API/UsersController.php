b5815233fcd9e2485b7be5b29a83202fbf76e0a4
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2020-03-07 12:58:12 -0500

    registro de usuario listo.

---
+++ app/Http/Controllers/API/UsersController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UsersController extends Controller
+{
+    public $successStatus = 200;
+    /**
+     * Register api
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        //Validate form request
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'identification_card' => 'required|unique:users,identification_card',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+        //Response fail te validation
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+
+        //Generate token access
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')-> accessToken;
+        $success['name'] =  $user->name;
+
+        return response()->json(['success'=>$success], $this->successStatus);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Passport
+Route::post('login', 'API\UsersController@login');
+Route::post('register', 'API\UsersController@register');
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('details', 'API\UsersController@details');
 });
 
-Route::get('list', 'NotesController@index');
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
